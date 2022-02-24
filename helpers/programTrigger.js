5499ab9e5c7f523a21a5216b65f086b0f38cae74
Sebastián Gámez <juan.gamez1001@gmail.com>
2022-02-24 15:53:09 -0500

    Translation complete

--- helpers/programTrigger.js
+++ helpers/programTrigger.js
@@ -7,7 +7,7 @@ const programTrigger = (arg, arg2) => {
     if (arg == "show") Program.showRequeriment(arg2);
     else if (arg == "create") Program.createInformationFile(arg2);
     else console.log(`
-        POR FAVOR USE EL COMANDO 'show' o 'create' PARA EJECUTAR EL PROGRAMA\n
+        PLEASE USE 'show' o 'create' COMMAND FOR RUN THE PROGRAM\n
     `.yellow.bold)
 
 }
